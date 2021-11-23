f4379e089c393207ec9c359352b1c3ff8725b5be
HyeonGyeongKim <ghwls9117@naver.com>
2021-11-23 20:41:15 +0900

    [Update] diet_result.html css 수정
    Former-commit-id: fdd997b4561557cd353ace587906cc34b4eb5003

--- app/static/js/checker.js
+++ app/static/js/checker.js
@@ -1,12 +1,16 @@
 //버튼 클릭마다 배경색 변경
+
 $(function(){
     let $label_chker = $(".label_checker");
 
     $label_chker.click(function(){
-        if($label_chker.hasClass("clicked")) {
-            $label_chker.removeClass("clicked")
-        }
-        $(this).addClass("clicked");
+        $(this).toggleClass("checked");
     });
-    
+
+});
+
+
+//정상 실행 확인 용
+$( function () {
+    alert('메시지를 띄웁니다.');
 });
