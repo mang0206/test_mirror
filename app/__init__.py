5f2808dc2e774a470363f9468ec1cca5d398c295
thsckdduq <thsckdduq@naver.com>
2021-12-02 13:50:55 +0900

    [Update] URI
    Former-commit-id: 2a6963503fcbdd8795dc5b8967e10876f4ed4746

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ydp123456@13.124.231.43:3306/diet"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://ydp01:ydp123456@ec2-3-34-125-24.ap-northeast-2.compute.amazonaws.com:3306/diet"
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:111111@localhost:3306/diet"
 app.config['JSON_AS_ASCII'] = False
 
