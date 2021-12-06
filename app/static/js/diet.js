a5b714013e91447c2715687be821988c4c7cf5ba
맹 민재 <alswosp0206@naver.com>
2021-12-06 15:11:01 +0900

    [Update] views.py add flash
    Former-commit-id: 1abac455f28b36b624bd244fa1973749147897aa

--- app/static/js/diet.js
+++ app/static/js/diet.js
@@ -24,10 +24,20 @@ $(function(){
 });
 
 // personal board의 submit 버튼 입력시 스크롤 이동
+// $(document).ready(function() {
+//     $(".move_button_hidden").on("click", function(event) {
+//         var offset = $("#move_funtion").offset();
+//         $('html body').animate({scrollTop : offset.top}, 1000);
+//     });
+// });
 $(document).ready(function() {
+    let nutrients = $('#personal_submit-data').data().name;
     $(".move_button_hidden").on("click", function(event) {
         var offset = $("#move_funtion").offset();
         $('html body').animate({scrollTop : offset.top}, 1000);
     });
 });
+// $(document).ready(function() {
+//     $('#test').val('원하는 값');
+// });
 
